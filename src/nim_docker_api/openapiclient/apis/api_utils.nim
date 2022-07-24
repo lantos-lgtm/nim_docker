a061ffd484cce77919c6c01a136877e18512a198
lantos-lgtm <l.leong1618@gmail.com>
2022-07-24 09:47:12 +0100

    remove unused

--- src/nim_docker_api/openapiclient/apis/api_utils.nim
+++ src/nim_docker_api/openapiclient/apis/api_utils.nim
@@ -11,39 +11,13 @@ import asyncstreams
 import asyncdispatch
 
 
-# template constructResult[T](response: Response): untyped =
-#   if response.code in {Http200, Http201, Http202, Http204, Http206}:
-#     try:
-#       when name(stripGenericParams(T.typedesc).typedesc) == name(Table):
-#         (some(json.to(parseJson(response.body), T.typedesc)), response)
-#       else:
-#         (some(marshal.to[T](response.body)), response)
-#     except JsonParsingError:
-#       # The server returned a malformed response though the response code is 2XX
-#       # TODO: need better error handling
-#       error("JsonParsingError")
-#       (none(T.typedesc), response)
-#   else:
-#     (none(T.typedesc), response)
-
-# parse hook to convert GO time.time.rfc3339nano to nim time
-proc parseHook*(s: string, i: var int, v: var DateTime) =
-    var str: string
-    try:
-        parseHook(s, i, str)
-    except ref JsonError:
-        raise newException(JsonError, "Invalid time format", )
-    # "0001-01-01T00:00:00Z"
-    try:
-        # go time.time probably has a bug and isn't always posting 9 bits of precision
-        v = str.parse("yyyy-MM-dd'T'HH':'mm':'ss'Z'")
-    except TimeParseError:
-        if str.len() != 30:
-            str = str[0..str.high-(30-str.len())]
-            str.add("0".repeat(29-str.len()) & "Z")
-        v = str.parse("yyyy-MM-dd'T'HH':'mm':'ss'.'fffffffff'Z'")
-
-
+type
+  Docker* = object
+    client*: HttpClient
+    basepath*: string
+  AsyncDocker* = object
+    client*: AsyncHttpClient
+    basepath*: string
 
 type
   DockerError* = object of CatchableError
@@ -52,6 +26,26 @@ type
   Conflict* = object of DockerError
   NotModified* = object of DockerError
   ServerError* = object of DockerError
+
+# something to help with boilerplate
+proc constructResult1*[T](response: Response | AsyncResponse): Future[T] {.multiSync.} =
+  case response.code():
+  of{Http200, Http201, Http202, Http204, Http206, Http304}:
+    when T is void:
+      return
+    elif T is Stream or T is FutureStream[string]:
+      return response.bodyStream
+    elif T is string:
+      return await response.body()
+    else:
+      return (await response.body()).fromJson(T.typedesc)
+  of Http400:
+    raise newException(BadRequest, await response.body())
+  of Http404:
+    raise newException(NotFound, await response.body())
+  else:
+    raise newException(ServerError, await response.body())
+
 
 template constructResult*[T](response: Response): untyped =
   if response.code in {Http200, Http201, Http202, Http204, Http206}:
@@ -95,6 +89,24 @@ macro encode*(dest: untyped, statements: untyped): untyped =
         )
       )
     result.add(exprNode)
+
+
+# parse hook to convert GO time.time.rfc3339nano to nim time
+proc parseHook*(s: string, i: var int, v: var DateTime) =
+    var str: string
+    try:
+        parseHook(s, i, str)
+    except ref JsonError:
+        raise newException(JsonError, "Invalid time format", )
+    # "0001-01-01T00:00:00Z"
+    try:
+        # go time.time probably has a bug and isn't always posting 9 bits of precision
+        v = str.parse("yyyy-MM-dd'T'HH':'mm':'ss'Z'")
+    except TimeParseError:
+        if str.len() != 30:
+            str = str[0..str.high-(30-str.len())]
+            str.add("0".repeat(29-str.len()) & "Z")
+        v = str.parse("yyyy-MM-dd'T'HH':'mm':'ss'.'fffffffff'Z'")
 
 
 # simple coerce hook. if 1st char is Uppercase -> 1st char lowercase objectParsing then coerces to lowercase
