1d0cca37b32ae22b62e82615b996927063dff013
lantos-lgtm <l.leong1618@gmail.com>
2022-07-27 21:19:37 +0100

    transpile

--- src/nim_docker_api/transpiler.nim
+++ src/nim_docker_api/transpiler.nim
@@ -4,10 +4,12 @@ import tables
 import sequtils
 import os
 
+
 var exportsTable = initTable[string, seq[string]]()
 var filesTable = initTable[string, string]()
-proc translateFile(path: string) =
-    let fileName = extractFilename(path)
+var filesFinalTable = initTable[string, string]()
+
+proc translateFile(filename, path: string) =
     var data = path.readFile()
 
     data.add("""
@@ -17,76 +19,98 @@ proc translateFile(path: string) =
     data = data.replace("##", "//")
     data = data.replace("#", "//")
     data = data.replace("*:", ":")
-    data = data.replace("type", "")
+    data = data.replace("type ", " ")
     data = data.replace("seq", "")
-    data = data.replace("{.pure.}", "")
+    data = data.replace("{.pure.} ", "")
     data = data.replace("import tables", "")
     data = data.replace("import times", "")
     data = data.replace("import jsony", "")
+    data = data.replace("`", "")
     data = data.replace("import options", "")
 
+    # map DateTime
     data = data.replace("DateTime", "Date")
-    let number_replace = @[ "uint64", "uint32","uint", "uint8","int64", "int32", "int8","int", "float64", "float32", "float"]
-    for val in number_replace:
-        data = data.replace(re(val&"(\\n|\\s+)?"), "Number$1")
 
+    # map numbers order matters
+    let number_replace = @[ "uint64", "uint32", "uint8", "uint", "int64", "int32", "int8","int", "float64", "float32", "float"]
+    for val in number_replace:
+        data = data.replace(re("\\s" & val & "(\\n|\\s+)?"), "Number$1")
+
+    # map boolean
     data = data.replace("bool", "Boolean")
 
+    # map types
     let interfaceMatch = re"((\n?\s\s?type)?|(^\s+)?)(\w+)\*?( ?= ?object)((.|\n)*?)(\n\n)"
-    let interfaceReplace = """
-
-    export interface $4 {
-    $6
-    }
-
-    """
+    let interfaceReplace = "\nexport interface $4 {\n$6\n}\n"
     data = data.replace(interfaceMatch, interfaceReplace)
 
-
+    # map tables
     let tableMatch = re"(?m)Table\[(\w+), (\w+)\]"
     let tableReplace = """{[key:$1]: $2}"""
     data = data.replace(tableMatch, tableReplace)
 
-    let enumMatch = re"(\n\s+?type\s+|\n\s+)(\w+).+= enum((.|\n)*?)(\n\n)"
-    let enumReplace = """
-
-
-    export enum $2 {
-        $3
-    }
-
-
-    """
+    # map enums
+    let enumMatch = re"(?m)^\s+(\w+)\* ?= ?enum((.|\n)*?)(^\n)"
+    let enumReplace = "\n\nexport enum $1 {\n$2\n}\n"
     data = data.replace(enumMatch, enumReplace)
 
+    # fix enums not having commas
+    let enumsFixMatch = re"""(\w+) = "(\w+)""""
+    discard """""""
+    let enumsFixReplace = """$1 = "$2","""
+    data = data.replace(enumsFixMatch, enumsFixReplace)
 
+    # remove x: Option[T] -> x?: T
     let optionFieldMatch = re"""(\n?\s+)(\w+)\*?: Option\[(.+)\]"""
     let optionFieldReplace = """$1$2?: $3"""
-
     data = data.replace(optionFieldMatch, optionFieldReplace)
-    echo data
 
+    # fix no mapping for Tables options
+    data = data.replace("Table[string, Option[{[key:string]: string}]]", "{[key:string]: {[key:string]: string}}")
     var exports: seq[string]
     for m in data.findAll(re"export \w+ (\w+)"):
         exports.add(m.group(0, data)[0])
 
-    exportsTable[path] = exports
-    filesTable[fileName] = data
 
-proc translateImports(text: string) =
+    exportsTable[filename] = exports
+    filesTable[filename] = data
+
+# dirty import mapper
+proc translateImports(filename: string, text: string) =
     var tempFile = text
 
+    # find all imports
     var importFiles: seq[string]
     for m in tempFile.findAll(re"import (\w+)"):
         importFiles.add(m.group(0, tempFile)[0])
 
+    let importMatch = re"import (\w+)"
+    let importReplace = ""
+    tempFile = tempFile.replace(importMatch, importReplace)
+
+    # if had mapping pull it in
     for importFile in  importFiles:
-        if exportsTable.hasKey(importFile):
-            let importString = "import {" & exportsTable[importFile].foldl(a & ", " & b & "} from" & importFile & ";\n")
+        if exportsTable.hasKey(importFile & ".nim"):
+            # create import { name } from "./file.nim"
+            let importStringsSaved = exportsTable[importFile & ".nim"]
+            let importString = "import {" & importStringsSaved.foldl(a & ", " & b ) & "} from \"./" & importFile & ".nim\";\n"
+          
             tempFile = importString & tempFile
 
-    echo tempFile
+    filesFinalTable[filename] = tempFile
 
 
-translateFile("src/nim_docker_api/openapiclient/models/model_container_stats.nim")
-translateImports("src/nim_docker_api/openapiclient/models/model_container_stats.nim")
+# translateFile("model_event_message", "src/nim_docker_api/openapiclient/models/model_event_message.nim")
+# translateImports("model_event_message", filesTable["model_event_message"])
+
+for file in walkDir("src/nim_docker_api/openapiclient/models/"):
+    translateFile(file.path.extractFilename, file.path)
+
+
+for filename, text in filesTable.pairs():
+    translateImports(filename, text)
+
+
+createDir("./typescript/")
+for (filename, text) in filesFinalTable.pairs():
+    writeFile("./typescript/" & filename & ".ts", text)
